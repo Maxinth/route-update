fe8c8dc6b54eb5117de0d8ab8b11d8c00d8c8ee5
maxinth <tunde.j.adeleye@gmail.com>
2022-02-04 09:53:27 +0100

    using relative paths

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -6,9 +6,17 @@ const User = () => {
     <>
       <div>Users </div>
       <nav className="nav">
-        <Link to="/user/profile">Profile</Link>
-        <Link to="/user/account">Account</Link>
+        {/* <Link to="/user/profile">Profile</Link>
+        <Link to="/user/account">Account</Link> */}
+
+        {/* using relative paths also work as the <Profile /> and <Account /> components are nested
+        within <User /> in the Routes in app.js */}
+
+        <Link to="profile">Profile</Link>
+        <Link to="account">Account</Link>
       </nav>
+
+      {/** HERE :  */}
       {/* nested components within the <Route /> in App.js show up here */}
       <Outlet />
       {/* nested components within the <Route /> in App.js show up here */}
